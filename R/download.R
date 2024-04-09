d2de6466915934d65e4ff255e664862d50177986
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-04-09 14:50:54 -0400

    Fix download
    The file name is now stored in the HTML text and not the `href`.
    
    Fixes <https://github.com/biobricks-ai/ctgov/issues/2>.

--- R/download.R
+++ R/download.R
@@ -1,15 +1,18 @@
 library(rvest)
 library(purrr)
 library(fs)
+library(stringr)
 
 options(timeout=1800) # download timeout
 
 # read html from page and grab file to download
 page   <- read_html("https://aact.ctti-clinicaltrials.org/pipe_files")
-href   <- page |> html_nodes("a") |> html_attr("href") 
-recent <- detect(href, ~ grepl("*.zip",.))
+nodes  <- page |> html_nodes("a")
+recent <- detect(nodes, ~ grepl("*.zip", . |> html_text() ))
 
 # download file to download directory
-download <- fs::dir_create("download") |> fs::path(fs::path_file(recent))
-url      <- paste0("https://aact.ctti-clinicaltrials.org",recent)
+name     <- recent |> html_text() |> stringr::str_trim()
+download <- fs::dir_create("download") |> fs::path(name)
+url      <- recent |> html_attr("href")
+print(paste("Downloading", url, "to", download))
 download.file(url,download)
