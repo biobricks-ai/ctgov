f03b6316a4649e03dbbf437eb7f9bbab9f19aca0
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-05-09 15:50:12 -0400

    Update flake.nix
    Add R packages and HTML::Parser.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,27 @@
 {
   description = "ClinicalTrials.gov BioBrick";
 
-  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    dev-shell.url = "github:biobricks-ai/dev-shell";
+  };
 
-  outputs = { self, dev-shell }: { devShells = dev-shell.devShells; };
+  outputs = { self, nixpkgs, flake-utils, dev-shell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
+          (oldAttrs:
+            let
+              perlDeps =
+                with perlPackages; [
+                  HTMLParser
+                ];
+	    in {
+            buildInputs = oldAttrs.buildInputs ++ [
+              perlDeps
+	      (with rPackages; [ arrow dplyr fs purrr readr vroom ])
+            ];
+          });
+      });
 }
